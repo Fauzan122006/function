af536cc29001a652370e5e649bde9cf28fbb2c5a
Fauzan <fauzantakibang@gmail.com>
2024-12-04 11:00:36 +0800

    menjumlahkanVolumeDuaKubus

---
+++ menjumlahkanVolumeKubus.js
@@ -0,0 +1,10 @@
+function menjumlahkanVolumeDuaKubus(a, b) {
+ let volumeA = Math.pow(a, 3)
+ let volumeB = Math.pow(b, 3)
+
+ return volumeA + volumeB
+
+}
+
+alert(menjumlahkanVolumeDuaKubus(8, 3))
+alert(menjumlahkanVolumeDuaKubus(10, 15))
